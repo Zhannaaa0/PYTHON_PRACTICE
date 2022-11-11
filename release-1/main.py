ccfac8313fe1fbe06ad6e9d32514172f2fa6de45
Zhanna Shtangrat <zshtangrat@mail.ru>
2022-11-11 22:52:13 +0300

    release 1 task 1

--- release-1/main.py
+++ release-1/main.py
@@ -1,2 +1,15 @@
 def hiFunction(name):
-    print()
+    return "Hi, " + name
+
+def sum(num1, num2):
+    return num1+num2
+
+def isEven(num):
+    return num%2==0
+
+def apples(num):
+    return "I have " + num + " apples"
+
+def getPower(num):
+    return num**2
+print(num(1,2))
